79fd8239cd14c686e77f0f044fc9b8fd78c40590
contextCue <14878694+ed42311@users.noreply.github.com>
2020-11-04 13:25:38 -0700

    added types from central perk

--- graphql.ts
+++ graphql.ts
@@ -5,18 +5,62 @@ import neo4j from 'neo4j-driver'
 const neo4jGraphqlJs = require('neo4j-graphql-js')
 const { makeAugmentedSchema } = neo4jGraphqlJs
 
-// Provide resolver functions for your schema fields
 const typeDefs = `
-type Kitteh {
-  name: String
-  cuteness: Int
-}
+  type Step {
+    latitude: Float
+    longitude: Float
+  }
+
+  type Tag {
+    key: String
+    value: String
+  }
+
+  type PointOfInterest {
+    name: String
+    location: Point
+    type: String
+    node_osm_id: ID!
+
+    wikipedia: String @cypher(statement: """
+      MATCH (this)-->(t:OSMTags)
+      WHERE EXISTS(t.wikipedia) WITH t LIMIT 1
+      CALL apoc.load.json('https://en.wikipedia.org/w/api.php?action=parse&prop=text&formatversion=2&format=json&page=' + apoc.text.urlencode(t.wikipedia)) YIELD value
+      RETURN value.parse.text
+    """)
+
+    tags: [Tag] @cypher(statement: """
+      MATCH (this)-->(t:OSMTags)
+      UNWIND keys(t) AS key
+      RETURN {key: key, value: t[key]} AS tag
+    """)
+
+    routeToPOI(poi: ID!): [Step] @cypher(statement: """
+      MATCH (other:PointOfInterest {node_osm_id: $poi})
+      CALL gds.alpha.shortestPath.stream({
+        nodeProjection: 'OSMNode',
+          relationshipProjection: {
+            ROUTE: {
+                type: 'ROUTE',
+                  properties: 'distance',
+                  orientation: 'UNDIRECTED'
+              }
+          },
+          startNode: this,
+          endNode: other,
+          relationshipWeightProperty: 'distance'
+      })
+      YIELD nodeId, cost
+      WITH gds.util.asNode(nodeId) AS node
+      RETURN {latitude: node.location.latitude, longitude: node.location.longitude} AS route
+    """)
+  }
 `
 
 const schema = makeAugmentedSchema({ typeDefs })
 const driver = neo4j.driver(
-  'bolt://18.208.119.122:32864',
-  neo4j.auth.basic('neo4j', 'floor-custody-directions')
+  'bolt://52.201.184.95:33525',
+  neo4j.auth.basic('neo4j', 'dwell-profit-subject')
 )
 
 const server = new ApolloServer({
